db01475e5835572afa93dbc7128c8371bb88a433
rrdai0712@gmail.com <rrdai0712@gmail.com>
2023-12-06 20:40:37 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -8,9 +9,11 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
 
+  answer = ""
   def Submit_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
+    answer = anvil.server.call('query',self.text_area_1.text )
+
+  text_area_2.text = answer
 
   def text_area_1_change(self, **event_args):
     """This method is called when the text in this text area is edited"""
